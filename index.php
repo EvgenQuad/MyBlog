95c357204191a1a3cb45cb3997e03b7cb4532ab3
EvgenS <mail4evgen@gmail.com>
2016-10-24 23:30:53 +0300

    un coment

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 <?php 
 	  echo 'asdasdasa';
-	// // FRONT CONTROLLER
+	// FRONT CONTROLLER
 
-	// // загальні налаштування
-	// ini_set('display_errors', 1);
-	// error_reporting(E_ALL);
+	// загальні налаштування
+	ini_set('display_errors', 1);
+	error_reporting(E_ALL);
 	
 
-	// // підключення файлів системи
-	// define ('ROOT', dirname(__FILE__));
-	// require_once(ROOT.'/components/Router.php');
-	// include_once(ROOT.'/components/Db.php');
+	// підключення файлів системи
+	define ('ROOT', dirname(__FILE__));
+	require_once(ROOT.'/components/Router.php');
+	include_once(ROOT.'/components/Db.php');
 		
 
-	// // виклик Router
-	// $router = new Router();
-	// $router->run();
+	// виклик Router
+	$router = new Router();
+	$router->run();
 
 
  ?> 
